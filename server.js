bd869560063d303ac1bb7a2b8b8e56b6f7ec7af3
Dylan Thomson <djthomson88@gmail.com>
2019-03-29 12:01:50 -0400

    Use routes in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require("./routes");
 
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -17,6 +18,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
