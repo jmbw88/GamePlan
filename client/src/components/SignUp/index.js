63a6b0a1bd79f3db4988113a5bcc437b78d7c4a9
Dylan Thomson <djthomson88@gmail.com>
2019-03-29 12:37:06 -0400

    Temporary handleSubmit

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -77,6 +77,25 @@ class SignUp extends Component {
   }
 
   handleSubmit = (event) => {
+    event.preventDefault();
+		console.log('sign-up-form, username: ');
+		console.log(this.state.username);
+		//request to server here
+		Axios.post('/user/', {
+			username: this.state.username,
+			password: this.state.password
+		})
+			.then(response => {
+				console.log(response);
+				if (!response.data.errmsg) {
+					console.log('successful signup');
+					this.setState({
+						redirectTo: '/login'
+					});
+				} else {
+					console.log('username already taken');
+				}
+			});
     // event.preventDefault();
     // if(this.validateForm()) {
     //   Axios.post("/user", {
