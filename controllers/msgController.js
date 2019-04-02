750df8903632cec50f70d28591c69cd08f7bcbbe
Dylan Thomson <djthomson88@gmail.com>
2019-04-02 14:31:02 -0400

    Find all messages

---
+++ controllers/msgController.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: (req, res) => {
+    db.Message.find({}).then((dbMsg) => {
+      res.json(dbMsg);
+    }).catch((err) => {
+      res.status(422).json(err);
+    });
+  },
+  findThread: (req, res) => {}
+}
--- routes/api/message.js
+++ routes/api/message.js
@@ -1,8 +1,10 @@
 const Router = require("express").Router();
 const msgController = require("../../controllers/msgController");
 
-// Router.route("/")
-//   .get(msgController.getAllMessages);
+Router.route("/")
+    .get(msgController.findAll);
+
+Router.route("/:userid/:")
 
 // Router.route("/:userid")
 //   .get(msgController.getThread)
