c78cd451574786ed466c0a0368d2e0f14a581fdb
Dylan Thomson <djthomson88@gmail.com>
2019-04-03 19:44:20 -0400

    Plot out invite routes

---
+++ controllers/inviteController.js
@@ -0,0 +1,44 @@
+const db = require("../models");
+
+module.exports = {
+  groupInvite: (req, res) => {
+
+  },
+
+  getGroupInvites: (req, res) => {
+
+  },
+
+  getUserGroupInvites: (req, res) => {
+
+  },
+
+  acceptGroupInvite: (req, res) => {
+
+  },
+
+  declineGroupInvite: (req, res) => {
+
+  },
+
+  eventInvite: (req, res) => {
+
+  },
+
+  getEventInvites: (req ,res) => {
+
+  },
+
+  getUserEventInvites: (req, res) => {
+
+  },
+
+  acceptEventInvite: (req, res) => {
+
+  },
+
+  declineEventInvite: (req, res) => {
+    
+  }
+
+}
--- routes/api/invite.js
+++ routes/api/invite.js
@@ -8,12 +8,26 @@ const inviteController = require("../../controllers/inviteController");
 // ACCEPT INVITE: deletes invite and adds them to the event/group
 // DECLINE INVITE: deletes invite (for now)
 
-// Router.route("/groups")
-//     .post(inviteController.inviteGroup)
-//     .delete(inviteController.deleteGroupInvite);
+Router.route("/groups")
+    .post(inviteController.groupInvite);
 
-// Router.route("/event")
-//     .post(inviteController.inviteEvent)
-//     .delete(inviteController.deleteEventInvite);
+Router.route("/groups/:id")
+    .get(inviteController.getGroupInvites);
+
+Router.route("/groups/user/:id")
+    .get(inviteController.getUserGroupInvites)
+    .put(inviteController.acceptGroupInvite)
+    .delete(inviteController.declineGroupInvite);
+
+Router.route("/event")
+    .post(inviteController.eventInvite);
+
+Router.route("/events/:id")
+    .get(inviteController.getEventInvites);
+
+Router.route("/events/user/:id")
+    .get(inviteController.getUserEventInvites)
+    .put(inviteController.acceptEventInvite)
+    .delete(inviteController.declineEventInvite);
 
 module.exports = Router;
