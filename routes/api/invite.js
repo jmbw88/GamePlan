0f459dbf3e174538fc27b50bc313df6655e1b663
Dylan Thomson <djthomson88@gmail.com>
2019-04-03 17:56:30 -0400

    Pseudocode invite routes

--- routes/api/invite.js
+++ routes/api/invite.js
@@ -1,6 +1,13 @@
 const Router = require("express").Router();
 const inviteController = require("../../controllers/inviteController");
 
+// INVITE ROUTES
+// CREATE
+// GET INVITES BY USER
+// GET INVITES FOR EVENT OR GROUP
+// ACCEPT INVITE: deletes invite and adds them to the event/group
+// DECLINE INVITE: deletes invite (for now)
+
 // Router.route("/groups")
 //     .post(inviteController.inviteGroup)
 //     .delete(inviteController.deleteGroupInvite);
