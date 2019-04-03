34c827752904dd427b3e47abc1ad9cb33a4b6bf3
Dylan Thomson <djthomson88@gmail.com>
2019-04-03 11:03:33 -0400

    findThread

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -8,7 +8,13 @@ module.exports = {
       res.status(422).json(err);
     });
   },
-  findThread: (req, res) => {},
+  findThread: (req, res) => {
+    db.Message.find({ $or: [{ to: userid, from: otherid }, { to: otherid, from: userid }] }).sort("createdAt").then((dbMsg) => {
+      res.json(dbMsg);
+    }).catch((err) => {
+      res.status(422).json(err);
+    });
+  },
   sendMsg: (req, res) => {
     db.Message.create(req.body).then((dbMsg) => {
       res.json(dbMsg);
