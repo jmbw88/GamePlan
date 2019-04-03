4532bd4e8f20e96d504dce1cfb2a7c91461bf09c
Dylan Thomson <djthomson88@gmail.com>
2019-04-03 15:04:11 -0400

    Comments because a couple of these functions are a bit complex

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -9,6 +9,7 @@ module.exports = {
     });
   },
 
+  // Find all messages user has sent/received for a particular user
   findThread: (req, res) => {
     const userid = req.params.userid;
     const otherid = req.params.otherid;
@@ -19,15 +20,19 @@ module.exports = {
     });
   },
 
+  // Return username and id for all users that this user has sent a message to or received a message from
   findContacts: (req, res) => {
     const id = req.params.id;
+    // Find all messages that user has sent or received
     db.Message.find({ $or: [{ to: id }, { from: id }] }).select("to from -_id").populate("to from").then((dbMsg) => {
+      // Return other user involved with message whether they are the sender or recipient
       const users = dbMsg.map((msg) => {
         if (String(msg.to._id) === id) {
           return {username: msg.from.account.username, id: msg.from._id};
         } else {
           return {username: msg.to.account.username, id: msg.to._id};
         }
+        // Remove duplicate users from array
       }).reduce((unique, o) => {
         if(!unique.some(obj => obj.username === o.username)) {
           unique.push(o);
