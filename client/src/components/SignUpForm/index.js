287b490ff9a61d94279e611e2309b1f5e1307534
Dylan Thomson <djthomson88@gmail.com>
2019-04-07 19:27:52 -0400

    Login and signup

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -125,73 +125,91 @@ class SignUpForm extends Component {
       return <Redirect to={{ pathname: this.state.redirectTo }}/>
     }
     return (
-      <div className="row">
-        <div className="col-12 mx-auto my-5">
-          <div className="card">
-            <div className="card-header">
-              <h2 className="text-center">Sign Up</h2>
-            </div>
-            <div className="card-body">
-              {this.state.errorMsg ? (
-                <div className="alert alert-danger" role="alert">
-                  {this.state.errorMsg}
-                </div>
-              ) : ""}
-              <form>
-                <div className="form-group">
-                  <label htmlFor="inputEmail">Email Address</label>
-                  <input type="email" 
-                         id="inputEmail" 
-                         placeholder="Enter email"
-                         name="email"
-                         value={this.state.email}
-                         onChange={this.handleChange}
-                         className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputUsername">Username</label>
-                  <input type="text" 
-                        id="inputUsername" 
-                        placeholder="Enter username"
-                        name="username"
-                        value={this.state.username}
-                        onChange={this.handleChange}
-                        className="form-control"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputPassword">Password</label>
-                  <input type="password" 
-                        id="inputPassword" 
-                        aria-describedby="passwordHelp"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange}
-                        className={"form-control" + (this.passwordsMatch() ? " border border-success" : "")}
-                  />
-                  <small id="passwordHelp" className="form-text text-muted">Password must be at least 8 characters</small>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="confirmPassword">Confirm Password</label>
-                  <input type="password" 
-                        id="confirmPassword"
-                        name="confirmPassword"
-                        value={this.confirmPassword}
-                        onChange={this.handleChange}
-                        className={"form-control" + (this.passwordsMatch() ? " border border-success" : "")}
-                  />
-                  {this.passwordsMatch() ? (
-                    <small className="form-text text-success">Passwords match</small>
-                  ) : ""}
-                </div>
-                <button className="btn btn-primary float-right" onClick={this.handleSubmit}>Submit</button>
-              </form>
-              <p>Already have an account? <Link to="/login">Login here.</Link></p>
-            </div>
-          </div>
+      <div id="signup">
+        <div id="logo">
+            <img src="assets/images/logo2.png"/>
         </div>
-      </div>
+        <div className="container">
+            <div id="signup-row" className="row justify-content-center align-items-center">
+                <div id="signup-column" className="col-md-6">
+                    <div id="signup-box" className="col-md-12">
+                        <form id="signup-form" className="form p-1" action="" method="post">
+                                <h4>Please fill in the form below to get started!</h4>
+                                {this.state.errorMsg ? (
+                                  <div className="alert alert-danger" role="alert">
+                                    {this.state.errorMsg}
+                                  </div>
+                                ) : ""}
+                                    <div id="signup-form">
+                                        <div className="form-group" id="email">
+                                          <label htmlFor="inputEmail">Email Address</label>
+                                          <input type="email" 
+                                                id="inputEmail" 
+                                                placeholder="Enter email"
+                                                name="email"
+                                                value={this.state.email}
+                                                onChange={this.handleChange}
+                                                className="form-control"
+                                          />
+                                            {/* <label for="email" className="text-info">Email Address:</label><br/>
+                                            <input type="text" name="email" id="email" className="form-control"/> */}
+                                        </div>
+                                        <div className="form-group" id="username">
+                                          <label htmlFor="inputUsername">Username</label>
+                                          <input type="text" 
+                                                id="inputUsername" 
+                                                placeholder="Enter username"
+                                                name="username"
+                                                value={this.state.username}
+                                                onChange={this.handleChange}
+                                                className="form-control"
+                                          />
+                                            {/* <label for="username" className="text-info">Username:</label><br/>
+                                            <input type="text" name="username" id="username" className="form-control"/> */}
+                                        </div>
+                                        <div className="form-group" id="password">
+                                          <label htmlFor="inputPassword">Password</label>
+                                          <input type="password" 
+                                                id="inputPassword" 
+                                                aria-describedby="passwordHelp"
+                                                name="password"
+                                                value={this.state.password}
+                                                onChange={this.handleChange}
+                                                className={"form-control" + (this.passwordsMatch() ? " border border-success" : "")}
+                                          />
+                                          <small id="passwordHelp" className="form-text text-muted">Password must be at least 8 characters</small>
+                                            {/* <label for="password" className="text-info">Password:</label><br/>
+                                            <input type="password" name="password" id="password" className="form-control"/> */}
+                                        </div>
+                                        <div className="form-group" id="password">
+                                          <label htmlFor="confirmPassword">Confirm Password</label>
+                                          <input type="password" 
+                                                id="confirmPassword"
+                                                name="confirmPassword"
+                                                value={this.confirmPassword}
+                                                onChange={this.handleChange}
+                                                className={"form-control" + (this.passwordsMatch() ? " border border-success" : "")}
+                                          />
+                                          {this.passwordsMatch() ? (
+                                            <small className="form-text text-success">Passwords match</small>
+                                          ) : ""}
+                                            {/* <label for="password" className="text-info">Confirm Password:</label><br/>
+                                            <input type="password" name="password" id="password" className="form-control"/> */}
+                                        </div>
+                                    </div>
+                                    <div className="submitBtn">
+                                      <button className="btn btn-info btn-md float-right mb-2" onClick={this.handleSubmit}>Sign Up</button>
+                                    </div>
+                                    {/* <p>Already have an account? <Link to="/login">Login here.</Link></p> */}
+                                    <div id="signIn" className="text-center">
+                                        <Link to="/login" className="text-info">Already have an account? Login here!</Link>
+                                    </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     );
   }
 }
