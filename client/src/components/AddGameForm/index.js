d65320bbaf6ee49888938d4f08cc685440f8d52b
unknown <djthomson88@gmail.com>
2019-04-09 19:12:57 -0400

    Testing autosuggest

--- client/src/components/AddGameForm/index.js
+++ client/src/components/AddGameForm/index.js
@@ -2,16 +2,122 @@ import React, { Component } from "react";
 import Axios from "axios";
 import { Redirect } from "react-router-dom";
 // import "./style.css";
+import Autosuggest from 'react-autosuggest';
+const languages = [
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'C#',
+    year: 2000
+  },
+  {
+    name: 'C++',
+    year: 1983
+  },
+  {
+    name: 'Clojure',
+    year: 2007
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  {
+    name: 'Go',
+    year: 2009
+  },
+  {
+    name: 'Haskell',
+    year: 1990
+  },
+  {
+    name: 'Java',
+    year: 1995
+  },
+  {
+    name: 'Javascript',
+    year: 1995
+  },
+  {
+    name: 'Perl',
+    year: 1987
+  },
+  {
+    name: 'PHP',
+    year: 1995
+  },
+  {
+    name: 'Python',
+    year: 1991
+  },
+  {
+    name: 'Ruby',
+    year: 1995
+  },
+  {
+    name: 'Scala',
+    year: 2003
+  }
+];
 
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+  
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp('^' + escapedValue, 'i');
+
+  return languages.filter(language => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <span>{suggestion.name}</span>
+  );
+}
 class AddGameForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      suggestions: [],
+      value: ''
+    };
   }
 
   componentDidMount() {
     this.getGames();
   }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
   getGames = () => {
     Axios.get("/api/games").then((res) => {
@@ -36,8 +142,29 @@ class AddGameForm extends Component {
     if(!this.props.loggedIn) {
       return <Redirect to={{ pathname: "/login" }}/>
     }
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: "Type 'c'",
+      value,
+      onChange: this.onChange
+    };
     return (
       <React.Fragment>
+              <Autosuggest 
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps} />
+      {/* <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      /> */}
         <h1>Add game</h1>
           {this.state.games ? this.state.games.map((game) => (
             <React.Fragment>
@@ -51,3 +178,237 @@ class AddGameForm extends Component {
 }
 
 export default AddGameForm;
+
+// const languages = [
+//   {
+//     name: 'C',
+//     year: 1972
+//   },
+//   {
+//     name: 'C#',
+//     year: 2000
+//   },
+//   {
+//     name: 'C++',
+//     year: 1983
+//   },
+//   {
+//     name: 'Clojure',
+//     year: 2007
+//   },
+//   {
+//     name: 'Elm',
+//     year: 2012
+//   },
+//   {
+//     name: 'Go',
+//     year: 2009
+//   },
+//   {
+//     name: 'Haskell',
+//     year: 1990
+//   },
+//   {
+//     name: 'Java',
+//     year: 1995
+//   },
+//   {
+//     name: 'Javascript',
+//     year: 1995
+//   },
+//   {
+//     name: 'Perl',
+//     year: 1987
+//   },
+//   {
+//     name: 'PHP',
+//     year: 1995
+//   },
+//   {
+//     name: 'Python',
+//     year: 1991
+//   },
+//   {
+//     name: 'Ruby',
+//     year: 1995
+//   },
+//   {
+//     name: 'Scala',
+//     year: 2003
+//   }
+// ];
+
+// // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+// function escapeRegexCharacters(str) {
+//   return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+// }
+
+// function getSuggestions(value) {
+//   const escapedValue = escapeRegexCharacters(value.trim());
+  
+//   if (escapedValue === '') {
+//     return [];
+//   }
+
+//   const regex = new RegExp('^' + escapedValue, 'i');
+
+//   return languages.filter(language => regex.test(language.name));
+// }
+
+// function getSuggestionValue(suggestion) {
+//   return suggestion.name;
+// }
+
+// function renderSuggestion(suggestion) {
+//   return (
+//     <span>{suggestion.name}</span>
+//   );
+// }
+
+// class App extends React.Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       value: '',
+//       suggestions: []
+//     };    
+//   }
+
+//   onChange = (event, { newValue, method }) => {
+//     this.setState({
+//       value: newValue
+//     });
+//   };
+  
+//   onSuggestionsFetchRequested = ({ value }) => {
+//     this.setState({
+//       suggestions: getSuggestions(value)
+//     });
+//   };
+
+//   onSuggestionsClearRequested = () => {
+//     this.setState({
+//       suggestions: []
+//     });
+//   };
+
+//   render() {
+//     const { value, suggestions } = this.state;
+//     const inputProps = {
+//       placeholder: "Type 'c'",
+//       value,
+//       onChange: this.onChange
+//     };
+
+//     return (
+//       <Autosuggest 
+//         suggestions={suggestions}
+//         onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+//         onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+//         getSuggestionValue={getSuggestionValue}
+//         renderSuggestion={renderSuggestion}
+//         inputProps={inputProps} />
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// import Autosuggest from 'react-autosuggest';
+
+// // Imagine you have a list of languages that you'd like to autosuggest.
+// const languages = [
+//   {
+//     name: 'C',
+//     year: 1972
+//   },
+//   {
+//     name: 'Elm',
+//     year: 2012
+//   },
+//   ...
+// ];
+
+// // Teach Autosuggest how to calculate suggestions for any given input value.
+// const getSuggestions = value => {
+//   const inputValue = value.trim().toLowerCase();
+//   const inputLength = inputValue.length;
+
+//   return inputLength === 0 ? [] : languages.filter(lang =>
+//     lang.name.toLowerCase().slice(0, inputLength) === inputValue
+//   );
+// };
+
+// // When suggestion is clicked, Autosuggest needs to populate the input
+// // based on the clicked suggestion. Teach Autosuggest how to calculate the
+// // input value for every given suggestion.
+// const getSuggestionValue = suggestion => suggestion.name;
+
+// // Use your imagination to render suggestions.
+// const renderSuggestion = suggestion => (
+//   <div>
+//     {suggestion.name}
+//   </div>
+// );
+
+// class Example extends React.Component {
+//   constructor() {
+//     super();
+
+//     // Autosuggest is a controlled component.
+//     // This means that you need to provide an input value
+//     // and an onChange handler that updates this value (see below).
+//     // Suggestions also need to be provided to the Autosuggest,
+//     // and they are initially empty because the Autosuggest is closed.
+//     this.state = {
+//       value: '',
+//       suggestions: []
+//     };
+//   }
+
+//   onChange = (event, { newValue }) => {
+//     this.setState({
+//       value: newValue
+//     });
+//   };
+
+//   // Autosuggest will call this function every time you need to update suggestions.
+//   // You already implemented this logic above, so just use it.
+//   onSuggestionsFetchRequested = ({ value }) => {
+//     this.setState({
+//       suggestions: getSuggestions(value)
+//     });
+//   };
+
+//   // Autosuggest will call this function every time you need to clear suggestions.
+//   onSuggestionsClearRequested = () => {
+//     this.setState({
+//       suggestions: []
+//     });
+//   };
+
+//   render() {
+//     const { value, suggestions } = this.state;
+
+//     // Autosuggest will pass through all these props to the input.
+//     const inputProps = {
+//       placeholder: 'Type a programming language',
+//       value,
+//       onChange: this.onChange
+//     };
+
+//     // Finally, render it!
+//     return (
+//       <Autosuggest
+//         suggestions={suggestions}
+//         onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+//         onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+//         getSuggestionValue={getSuggestionValue}
+//         renderSuggestion={renderSuggestion}
+//         inputProps={inputProps}
+//       />
+//     );
+//   }
+// }
